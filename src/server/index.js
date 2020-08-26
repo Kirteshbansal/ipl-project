aecfd9529c9a9ba2e5f0edbccd1ba9267c3aed03
Kirtesh Bansal <kirtesh.13@mountblue.tech>
2020-08-26 13:44:13 +0530

    Add path module & format code flow

--- src/server/index.js
+++ src/server/index.js
@@ -1,53 +1,56 @@
 const fs = require("fs");
 const csv = require("csvtojson");
-const MATCHES_FILE_PATH = "./../data/matches.csv";
-const DELIVERIES_FILE_PATH = "./../data/deliveries.csv";
+const path = require("path");
+
 const teamWonTossAndMatch = require("./problems/teamWonTossAndMatch");
 const highestTimesPOMPerSeason = require("./problems/highestTimesPOMPerSeason");
 const strikeRateOfMSDhoni = require("./problems/strikeRateOfMSDhoni");
 const highestTimesDissmissedPlayer = require("./problems/highestTimesDissmissedPlayer");
 const bestSuperOverEconomyBowler = require("./problems/bestSuperOverEconomyBowler");
-const DATA_JSON_FILE_PATH1 = "./../public/output/teamWonTossAndMatch.json";
-const DATA_JSON_FILE_PATH2 = "./../public/output/highestTimesPOMPerSeason.json";
-const DATA_JSON_FILE_PATH3 = "./../public/output/strikeRateOfMSDhoni.json";
-const DATA_JSON_FILE_PATH4 = "./../public/output/highestTimesDissmissedPlayer.json";
-const DATA_JSON_FILE_PATH5 = "./../public/output/bestSuperOverEconomyBowler.json";
-function main(){
-    csv()
-      .fromFile(MATCHES_FILE_PATH)
-      .then(matches => {
-        csv()
+
+const MATCHES_FILE_PATH = path.join(__dirname, "../data/matches.csv");
+const DELIVERIES_FILE_PATH = path.join(__dirname, "../data/deliveries.csv");
+const DATA_JSON_FILE_PATH1 = path.join(__dirname, "../public/output/teamWonTossAndMatch.json");
+const DATA_JSON_FILE_PATH2 = path.join(__dirname, "../public/output/highestTimesPOMPerSeason.json");
+const DATA_JSON_FILE_PATH3 = path.join(__dirname, "../public/output/strikeRateOfMSDhoni.json");
+const DATA_JSON_FILE_PATH4 = path.join(__dirname, "../public/output/highestTimesDissmissedPlayer.json");
+const DATA_JSON_FILE_PATH5 = path.join(__dirname, "../public/output/bestSuperOverEconomyBowler.json");
+function main() {
+  csv()
+    .fromFile(MATCHES_FILE_PATH)
+    .then((matches) => {
+      csv()
         .fromFile(DELIVERIES_FILE_PATH)
-        .then(deliveries => {
+        .then((deliveries) => {
           let result = teamWonTossAndMatch(matches);
-          saveData("teamWonTossAndMatch",result,DATA_JSON_FILE_PATH1)
+          saveData("teamWonTossAndMatch", result, DATA_JSON_FILE_PATH1);
           let result1 = highestTimesPOMPerSeason(matches);
-          saveData("highestTimesPOMPerSeason",result1,DATA_JSON_FILE_PATH2)
-          let result2 = strikeRateOfMSDhoni(matches,deliveries);
-          saveData("strikeRateOfMSDhoni",result2,DATA_JSON_FILE_PATH3);
-          let result3 = highestTimesDissmissedPlayer(matches,deliveries);
-          saveData("highestTimesDissmissedPlayer",result3,DATA_JSON_FILE_PATH4);
+          saveData("highestTimesPOMPerSeason", result1, DATA_JSON_FILE_PATH2);
+          let result2 = strikeRateOfMSDhoni(matches, deliveries);
+          saveData("strikeRateOfMSDhoni", result2, DATA_JSON_FILE_PATH3);
+          let result3 = highestTimesDissmissedPlayer(deliveries);
+          saveData(
+            "highestTimesDissmissedPlayer",
+            result3,
+            DATA_JSON_FILE_PATH4
+          );
           let result4 = bestSuperOverEconomyBowler(deliveries);
-          saveData("bestSuperOverEconomyBowler",result4,DATA_JSON_FILE_PATH5);
-   })
-  })
-};
+          saveData("bestSuperOverEconomyBowler", result4, DATA_JSON_FILE_PATH5);
+        });
+    });
+}
 
-
-function saveData(key,result,DATA_JSON_FILE_PATH){
-  const jsonData = {
-  }
+function saveData(key, result, DATA_JSON_FILE_PATH) {
+  const jsonData = {};
 
   jsonData[key] = result;
 
   const jsonString = JSON.stringify(jsonData);
-  fs.writeFile(DATA_JSON_FILE_PATH, jsonString, "utf8", err => {
+  fs.writeFile(DATA_JSON_FILE_PATH, jsonString, "utf8", (err) => {
     if (err) {
       console.error(err);
     }
   });
 }
-
-
 
 main();
