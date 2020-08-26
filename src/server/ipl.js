01739d8aef67341c12286bc8c4b5b5bb6caf1b54
Kirtesh Bansal <kirtesh.13@mountblue.tech>
2020-08-26 23:23:16 +0530

    Add highestTimesPOMPerSeason function

--- src/server/ipl.js
+++ src/server/ipl.js
@@ -14,6 +14,32 @@ function teamWonTossAndMatch(matches) {
   }, {});
 }
 
+//   Highest times player of the match per season
+function highestTimesPOMPerSeason(matches) {
+  return Object.entries(
+    matches.reduce((data, match) => {
+      let season = match["season"];
+      let playerOfMatch = match["player_of_match"];
+      if (data[season]) {
+        if (data[season][playerOfMatch]) {
+          data[season][playerOfMatch] += 1;
+        } else {
+          data[season][playerOfMatch] = 1;
+        }
+      } else {
+        data[season] = {};
+        data[season][playerOfMatch] = 1;
+      }
+      return data;
+    }, {})
+  ).reduce((result, match) => {
+    let max = Object.entries(match[1]).sort((a, b) => b[1] - a[1])[0];
+    result[match[0]] = max;
+    return result;
+  }, {});
+}
+
 module.exports = {
   teamWonTossAndMatch: teamWonTossAndMatch,
+  highestTimesPOMPerSeason: highestTimesPOMPerSeason,
 };
