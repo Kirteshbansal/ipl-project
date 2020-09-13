6b2d13388e2eca25b6cdf0b4f9e26eda56698f00
Kirtesh Bansal <kirtesh.13@mountblue.tech>
2020-09-13 14:50:50 +0530

    Update basic setup of app.js file

--- src/public/app.js
+++ src/public/app.js
@@ -1,93 +1,14 @@
-async function visualizeTeamWonTossAndMatch() {
-  let data = await fetch("./output/teamWonTossAndMatch.json")
+function fetchAndVisualizeData() {
+  fetch("./output/data.json")
     .then((response) => response.json())
-    .then((response) => response.teamWonTossAndMatch);
-
-  const teamData = Object.entries(data);
-
-  const angle = -45;
-  CommonHighChart(
-    "teams-won-matches-and-toss",
-    "1. Number of Times Each Team Won Both Toss And Match",
-    "Number of times",
-    "Won toss & match",
-    teamData,
-    angle
-  );
+    .then(visualizeData);
 }
 
-visualizeTeamWonTossAndMatch();
+fetchAndVisualizeData();
 
-async function visualizehighestTimesPOMPerSeason() {
-  let data = await fetch("./output/highestTimesPOMPerSeason.json")
-    .then((response) => response.json())
-    .then((response) => response.highestTimesPOMPerSeason)
-    .then((response) => Object.entries(response))
-    .then((response) =>
-      response.map((response) => ({
-        name: response[1][0],
-        data: [[response[0], response[1][1]]],
-      }))
-    );
+function visualizeData(data) {}
 
-  Highcharts.chart("player-of-the-match-per-season", {
-    chart: {
-      type: "column",
-    },
-    title: {
-      text: "2. Highest Times Player of The Match Per Season",
-    },
-    xAxis: {
-      type: "category",
-    },
-    yAxis: {
-      min: 0,
-      title: {
-        text: "Number of times",
-      },
-    },
-    tooltip: {
-      headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-      pointFormat:
-        '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-        '<td style="padding:0"><b>{point.y:.f} times</b></td></tr>',
-      footerFormat: "</table>",
-      shared: true,
-      useHTML: true,
-    },
-    plotOptions: {
-      column: {
-        pointPadding: 1,
-        borderWidth: 5,
-      },
-    },
-    series: data,
-  });
-}
-
-visualizehighestTimesPOMPerSeason();
-
-async function visualizestrikeRateOfPlayer() {
-  let data = await fetch("./output/strikeRateOfPlayer.json")
-    .then((response) => response.json())
-    .then((response) => response["strikeRateOfMS Dhoni"]);
-
-  const playerData = Object.entries(data);
-
-  const angle = 0;
-  CommonHighChart(
-    "strike-rate-of-player",
-    "3. Strike Rate of MS Dhoni Per Season",
-    "Strike rate",
-    "Strike rate",
-    playerData,
-    0
-  );
-}
-
-visualizestrikeRateOfPlayer();
-
-function CommonHighChart(container, title, yAxisTitle, name, data, angle) {
+function CommonHighChart(container, title, yAxisTitle, name, data, angle = 0) {
   Highcharts.chart(container, {
     chart: {
       type: "column",
