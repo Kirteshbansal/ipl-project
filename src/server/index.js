8c40b75ca95ec1a8b2c7cf013c2b56fbb4617538
Kirtesh Bansal <kirtesh.13@mountblue.tech>
2020-09-13 10:57:22 +0530

    Update index.js file with mysql module's function

--- src/server/index.js
+++ src/server/index.js
@@ -1,50 +1,18 @@
-const fs = require("fs");
-const csv = require("csvtojson");
-const path = require("path");
-
+const connection = require("./config");
+const dataProcess = require("./dataProcess");
 const ipl = require("./ipl");
 
-const MATCHES_FILE_PATH = path.join(__dirname, "../data/matches.csv");
-const DELIVERIES_FILE_PATH = path.join(__dirname, "../data/deliveries.csv");
-const DATA_JSON_FILE_PATH1 = path.join(__dirname, "../public/output/teamWonTossAndMatch.json");
-const DATA_JSON_FILE_PATH2 = path.join(__dirname, "../public/output/highestTimesPOMPerSeason.json");
-const DATA_JSON_FILE_PATH3 = path.join(__dirname, "../public/output/strikeRateOfPlayer.json");
-const DATA_JSON_FILE_PATH4 = path.join(__dirname, "../public/output/highestTimesDissmissedPlayer.json");
-const DATA_JSON_FILE_PATH5 = path.join(__dirname, "../public/output/bestSuperOverEconomyBowler.json");
-
-function main() {
-  csv()
-    .fromFile(MATCHES_FILE_PATH)
-    .then((matches) => {
-      csv()
-        .fromFile(DELIVERIES_FILE_PATH)
-        .then((deliveries) => {
-          let result = ipl.teamWonTossAndMatch(matches);
-          saveData("teamWonTossAndMatch", result, DATA_JSON_FILE_PATH1);
-          let result1 = ipl.highestTimesPOMPerSeason(matches);
-          const player = "MS Dhoni";
-          saveData("highestTimesPOMPerSeason", result1, DATA_JSON_FILE_PATH2);
-          let result2 = ipl.strikeRateOfPlayer(matches, deliveries,player);
-          saveData(`strikeRateOf${player}`, result2, DATA_JSON_FILE_PATH3);
-          let result3 = ipl.highestTimesDissmissedPlayer(deliveries);
-          saveData("highestTimesDissmissedPlayer",result3,DATA_JSON_FILE_PATH4);
-          let result4 = ipl.bestSuperOverEconomyBowler(deliveries);
-          saveData("bestSuperOverEconomyBowler", result4, DATA_JSON_FILE_PATH5);
-        });
-    });
-}
-
-function saveData(key, result, DATA_JSON_FILE_PATH) {
-  const jsonData = {};
-
-  jsonData[key] = result;
-
-  const jsonString = JSON.stringify(jsonData);
-  fs.writeFile(DATA_JSON_FILE_PATH, jsonString, "utf8", (err) => {
+async function main() {
+  connection.connect(function (err) {
     if (err) {
-      console.error(err);
+      console.error("error connecting: " + err.stack);
+      return;
     }
+    console.log("connected as id " + connection.threadId);
   });
+  const result = await Promise.all([]);
+  dataProcess.saveData(result);
+  connection.end();
 }
 
-main();
+main().catch(console.error);
