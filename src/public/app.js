5c40c34a092dc8336c3f7b82fe39848fee0d0e68
Kirtesh Bansal <kirtesh.13@mountblue.tech>
2020-09-13 14:53:12 +0530

    Add visualizeMatchesPlayedPerYear() function

--- src/public/app.js
+++ src/public/app.js
@@ -6,7 +6,21 @@ function fetchAndVisualizeData() {
 
 fetchAndVisualizeData();
 
-function visualizeData(data) {}
+function visualizeData(data) {
+  visualizeMatchesPlayedPerYear(data.matchesPlayedPerYear);
+}
+
+// visualizeMatchesPlayedPerYear
+function visualizeMatchesPlayedPerYear(inputData) {
+  const seriesData = inputData.map((el) => Object.values(el));
+  CommonHighChart(
+    "matches-played-per-year",
+    "1. Total Number of Matches Played Per Year",
+    "No. of Matches",
+    "Years",
+    seriesData
+  );
+}
 
 function CommonHighChart(container, title, yAxisTitle, name, data, angle = 0) {
   Highcharts.chart(container, {
