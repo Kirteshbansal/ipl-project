e9286983c8762c637bb0d5fe3b77136d9aef6056
Kirtesh Bansal <kirtesh.13@mountblue.tech>
2020-09-13 11:22:45 +0530

    Add extraRunsConcededByEachTeam() function

--- src/server/ipl.js
+++ src/server/ipl.js
@@ -14,7 +14,15 @@ function matchesWonByEachTeam() {
   return dataProcess.queryExecuter(matchesWonByEachTeamQuery);
 }
 
+// Extra runs conceded per team in the year 2016
+function extraRunsConcededByEachTeam() {
+  const extraRunsConcededByEachTeamQuery =
+    "SELECT `bowling_team`, SUM(`extra_runs`) AS extra_runs_conceded FROM `deliveries` LEFT JOIN matches ON match_id = id WHERE season = 2016 GROUP BY `bowling_team`";
+  return dataProcess.queryExecuter(extraRunsConcededByEachTeamQuery);
+}
+
 module.exports = {
   matchesPlayedPerYear: matchesPlayedPerYear,
   matchesWonByEachTeam: matchesWonByEachTeam,
+  extraRunsConcededByEachTeam: extraRunsConcededByEachTeam,
 };
