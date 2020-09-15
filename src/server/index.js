d5f23c43a2ea49e988f36279015ae6c3a7b5ad7d
Kirtesh Bansal <kirtesh.13@mountblue.tech>
2020-09-15 23:12:13 +0530

    Update main() function

--- src/server/index.js
+++ src/server/index.js
@@ -1,25 +1,13 @@
-const connection = require("./config");
 const dataProcess = require("./dataProcess");
 const ipl = require("./ipl");
 
 async function main() {
-  connection.connect(function (err) {
-    if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
-    }
-    console.log("connected as id " + connection.threadId);
-  });
-  const result = await Promise.all([
-    ipl.matchesPlayedPerYear(),
-    ipl.matchesWonByEachTeam(),
-    ipl.extraRunsConcededByEachTeam(),
-    ipl.economicalBowlers(),
-    ipl.teamWonTossAndMatch(),
-    ipl.strikeRateOfPlayer(),
-  ]);
-  dataProcess.saveData(result);
-  connection.end();
+  try {
+    const result = await Promise.all([]);
+    dataProcess.saveData(result);
+  } catch (err) {
+    throw err;
+  }
 }
 
 main().catch(console.error);
