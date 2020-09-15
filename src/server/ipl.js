787e57456ee42d3b733bd597fdfe29f7ab91d3d8
Kirtesh Bansal <kirtesh.13@mountblue.tech>
2020-09-15 23:11:35 +0530

    Add matchesPlayedPerYear() function

--- src/server/ipl.js
+++ src/server/ipl.js
@@ -2,4 +2,23 @@ const sequelize = require("sequelize");
 
 const matches = require("./models/match");
 
-module.exports = {};
+// Number of matchhes played per year
+async function matchesPlayedPerYear() {
+  try {
+    return await matches
+      .findAll({
+        attributes: [
+          "season",
+          [sequelize.fn("COUNT", sequelize.col("id")), "total_matches"],
+        ],
+        group: "season",
+      })
+      .then((d) => JSON.parse(JSON.stringify(d)));
+  } catch (err) {
+    throw err;
+  }
+}
+
+module.exports = {
+  matchesPlayedPerYear: matchesPlayedPerYear,
+};
