eb5cd0b88f5c0e1ddc1cce3ecbf5f4eb806b8c99
Kirtesh Bansal <kirtesh.13@mountblue.tech>
2020-09-15 23:22:10 +0530

    Add matchesWonByEachTeam() function's invocation

--- src/server/index.js
+++ src/server/index.js
@@ -3,7 +3,10 @@ const ipl = require("./ipl");
 
 async function main() {
   try {
-    const result = await Promise.all([ipl.matchesPlayedPerYear()]);
+    const result = await Promise.all([
+      ipl.matchesPlayedPerYear(),
+      ipl.matchesWonByEachTeam(),
+    ]);
     dataProcess.saveData(result);
   } catch (err) {
     throw err;
