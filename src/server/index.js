b45e039b654249a18771d341213b973348081359
Kirtesh Bansal <kirtesh.13@mountblue.tech>
2020-09-15 23:15:23 +0530

    Add matchesPlayedPerYear() function's invocation

--- src/server/index.js
+++ src/server/index.js
@@ -3,7 +3,7 @@ const ipl = require("./ipl");
 
 async function main() {
   try {
-    const result = await Promise.all([]);
+    const result = await Promise.all([ipl.matchesPlayedPerYear()]);
     dataProcess.saveData(result);
   } catch (err) {
     throw err;
