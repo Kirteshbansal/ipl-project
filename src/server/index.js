3a821d4595dc3337e54aa7142b41ff1abe5a46c7
Kirtesh Bansal <kirtesh.13@mountblue.tech>
2020-09-17 12:29:32 +0530

    Add connection authentication and close statement

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,11 @@
+const connection = require("./config");
 const dataProcess = require("./dataProcess");
 const ipl = require("./ipl");
 
 async function main() {
   try {
+    await connection.authenticate();
+    console.log("connection successful");
     const result = await Promise.all([
       ipl.matchesPlayedPerYear(),
       ipl.matchesWonByEachTeam(),
@@ -10,6 +13,8 @@ async function main() {
       ipl.economicalBowlers(),
     ]);
     dataProcess.saveData(result);
+    await connection.close();
+    console.log("connection closed");
   } catch (err) {
     throw err;
   }
